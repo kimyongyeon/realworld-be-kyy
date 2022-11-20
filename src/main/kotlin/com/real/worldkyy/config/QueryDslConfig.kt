700da31211d8e5a09c50507daacea92aee7cc0d3
yongyeonkim <es-kyy@smiltegate.com>
2022-11-20 13:04:44 +0900

    querydsl 테스트 코드 추가

--- src/main/kotlin/com/real/worldkyy/config/QueryDslConfig.kt
+++ src/main/kotlin/com/real/worldkyy/config/QueryDslConfig.kt
@@ -1,13 +1,13 @@
 package com.real.worldkyy.config
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import javax.persistence.EntityManager
-import javax.persistence.PersistenceContext
 
 @Configuration
-class QueryDslConfig constructor(@PersistenceContext private val em: EntityManager) {
+class QueryDslConfig constructor(@Autowired private val em: EntityManager) {
     @Bean
     fun query(): JPAQueryFactory {
         return JPAQueryFactory(em)
--- src/test/kotlin/com/real/worldkyy/service/customer/CustomerServiceTest.kt
+++ src/test/kotlin/com/real/worldkyy/service/customer/CustomerServiceTest.kt
@@ -1,6 +1,8 @@
 package com.real.worldkyy.service.customer
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import com.real.worldkyy.domain.Customer
+import com.real.worldkyy.domain.QCustomer.customer
 import com.real.worldkyy.response.CustomerRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,17 +11,26 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 internal class CustomerServiceTest @Autowired constructor(
     // Autowired를 안하면 주입이 되지 않음.
-    val customerRepository: CustomerRepository
+    val customerRepository: CustomerRepository,
+    val query: JPAQueryFactory
 ) {
 
     @Test
     fun usernameTest() {
-//        val findUserName = customerService.findUserName("kkk")
+        val fetch = query.select(customer)
+            .from(customer)
+            .where(customer.username.eq("hello"))
+            .fetch()
+        print(fetch)
     }
 
     @Test
     fun customerFindEmailTest() {
-//        val email = customerService.findEmail("test@naver.com")
+        val fetch = query.select(customer)
+            .from(customer)
+            .where(customer.email.eq("test@naver.com"))
+            .fetch()
+        print(fetch)
     }
 
     @Test
